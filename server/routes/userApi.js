b34e243be4b2621a8f3b19b1a57bfd0782ba328c
Navneet007 <navneetjustin@gmail.com>
2021-08-07 00:04:31 +0530

    before db saving part

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
+router.get("/", (req, res) => res.send("users router"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail().isEmail(),
+    check("password", "Password should have at least 5  charcters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
-router.get("/", (req, res)=>res.send("users router"));
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("users router post method");
+  }
+);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 5000;
 connectToDB();
 
 //define routes and API
+app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/userApi"));
-app.use("/api/products", require("./routes/productApi"))
+app.use("/api/products", require("./routes/productApi"));
 
+app.get("/", (req, res) => {
+  res.send("my app is up");
+});
 
-app.get("/", (req, res)=>{
-    res.send("my app is up");
-})
-
-app.listen(PORT, ()=>{
-    console.log(`server is listening at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is listening at port ${PORT}`);
+});
